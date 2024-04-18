7ffc6f8f0d9179d192457d05d61a093156b39c4b
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2024-04-18 19:45:49 +0300

    add top bar

--- app/src/main/java/com/example/navigationtestapp/MainActivity.kt
+++ app/src/main/java/com/example/navigationtestapp/MainActivity.kt
@@ -3,12 +3,31 @@ package com.example.navigationtestapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.TopAppBarState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import com.example.common_core.util.Until.fromJson
 import com.example.details.domain.model.ProductItem
 import com.example.details.presentation.DetailScreen
 import com.example.navigation.AppNavigation
 import com.example.navigation.Navigator
-import com.example.product.domain.models.Product
 import com.example.product.presentation.screen.ProductListScreen
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -20,17 +39,55 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var navigator: Navigator
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppNavigation(
-                navigator = navigator,
-                detailScreen = {
-                    val item = it.fromJson(ProductItem::class.java)
-                    DetailScreen(item)
+            var title by remember { mutableStateOf("Home") }
+            var visibleBack by remember { mutableStateOf(false) }
+            val
+                    scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+            Scaffold(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .nestedScroll(scrollBehavior.nestedScrollConnection),
+                topBar = {
+                    CenterAlignedTopAppBar(
+                        title = {
+                            Text(text = title)
+                        },
+                        navigationIcon = {
+                            if (visibleBack) {
+                                IconButton(onClick = { navigator.goBack() }) {
+                                    Icon(
+                                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                        contentDescription = "Back"
+                                    )
+                                }
+                            }
+                        },
+                        actions = {
+
+                        },
+                        scrollBehavior = scrollBehavior
+                    )
                 },
-                listScreen = {
-                    ProductListScreen()
+                content = { paddingValues ->
+                    AppNavigation(
+                        navigator = navigator,
+                        detailScreen = {
+                            val item = it.fromJson(ProductItem::class.java)
+                            title = "Detail Screen"
+                            visibleBack = true
+                            DetailScreen(item)
+
+                        },
+                        listScreen = {
+                            title = "List Screen"
+                            visibleBack = false
+                            ProductListScreen(paddingValues)
+                        }
+                    )
                 }
             )
         }
--- product/src/main/java/com/example/product/presentation/screen/ProductListScreen.kt
+++ product/src/main/java/com/example/product/presentation/screen/ProductListScreen.kt
@@ -7,27 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.LargeTopAppBar
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.common_core.components.ErrorComponent
 import com.example.common_core.components.ErrorComponentTest
@@ -36,78 +22,35 @@ import com.example.product.domain.until.DataError
 import com.example.product.presentation.event.ListUIEvent
 import com.example.product.presentation.viewmodel.ListViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ProductListScreen() {
+fun ProductListScreen(paddingValues: PaddingValues) {
     val viewModel: ListViewModel = hiltViewModel()
     val stateUi by viewModel.uiState.collectAsState()
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { "Products List" },
-                actions = {
-                    IconButton(onClick = { }) {
-                        Icon(imageVector = Icons.Filled.Settings, contentDescription = "menu")
-                    }
+    LaunchedEffect(true) {
+        viewModel.onEvent(ListUIEvent.GetList)
+    }
+
+    when {
+        stateUi.isLoading -> LoadingComponent()
+
+        stateUi.error != null -> ErrorComponent(error = stateUi.error)
+
+        stateUi.errorMsg != null -> {
+            when (val error = stateUi.errorMsg) {
+                is DataError.NetworkError -> {
+                    ErrorComponentTest(error = error.name)
                 }
-            )
-        }
-    ) {innerPadding ->
-
-        LaunchedEffect(true) {
-            viewModel.onEvent(ListUIEvent.GetList)
-        }
-
-        when {
-            stateUi.isLoading -> LoadingComponent()
-
-            stateUi.error != null -> ErrorComponent(error = stateUi.error)
-
-            stateUi.errorMsg != null -> {
-                when(val error = stateUi.errorMsg) {
-                    is DataError.NetworkError -> {
-                        ErrorComponentTest(error = error.name)
-                    }
-                    else -> {
-
-                    }
-                }
+                else -> {}
             }
-            stateUi.listData != null -> {
-                VerticalSection(stateUi.listData?.productList ?: emptyList(), innerPadding) {
-                    viewModel.onEvent(ListUIEvent.ProductClicked(it))
-                }
+        }
+
+        stateUi.listData != null -> {
+            VerticalSection(stateUi.listData?.productList ?: emptyList(), paddingValues) {
+                viewModel.onEvent(ListUIEvent.ProductClicked(it))
             }
         }
     }
-
-
-
-//    Scaffold(
-//        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-//
-//        topBar = {
-//            LargeTopAppBar(
-//                title = { "Test top bar" },
-//                colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Gray),
-//                navigationIcon = {
-//                    IconButton(onClick = { }) {
-//                        Icon(imageVector = Icons.Filled.Menu, contentDescription = "menu")
-//                    }
-//                },
-//                scrollBehavior = scrollBehavior
-//            )
-//        },
-//        content = {
-//
-//        }
-//
-//    )
-
-
-
-
 
     BackHandler(enabled = true) {
 //        viewModel.onEvent(ListUIEvent.Dismiss)
@@ -116,9 +59,13 @@ fun ProductListScreen() {
 
 
 @Composable
-fun VerticalSection(productList: List<Product>, innerPaddingValues: PaddingValues ,onEvent: (productItem: Product) -> Unit) {
+fun VerticalSection(
+    productList: List<Product>,
+    paddingValues: PaddingValues,
+    onEvent: (productItem: Product) -> Unit
+) {
     // val products = remember { section.productItem }
-    LazyColumn(modifier = Modifier.padding(innerPaddingValues)) {
+    LazyColumn(modifier = Modifier.padding(paddingValues)) {
         items(items = productList, key = { it.productId }) { productItem ->
             VerticalItemCard(
                 item = productItem,
